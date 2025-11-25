c0ffcb64b6dafc511b2b37d5cf349573b28fe92f
Ivan <sartonev.ivan@gmail.com>
2025-11-25 13:55:04 +0200

    domrab

--- zad2.py
+++ zad2.py
@@ -1,10 +1,12 @@
 def reverse_string(str):
     result = ""
+    #obhojda stringa i dobavq tekushtiqt simvol otpred
     for i in str:
         result = i + result
     return result
 
 def reverse_string_rec(str):
+    #proverqva dali str e prazen
     if str == "":
         return str
     return reverse_string_rec(str[1:]) + str[0]
